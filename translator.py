b35ff93555f7ae420a48196ebbdad61b332bde7a
Marcelus Easley <marcellus.easley@gmail.com>
2016-06-19 04:05:56 -0400

    add imports, api endpoint, admclient call, headers, and sample xml

---
+++ translator.py
@@ -0,0 +1,49 @@
+#
+# Filename: translator.py
+# 6/19/2016 12:09 AM
+#
+#
+from adm import AdmOAuthClient
+from bs4 import BeautifulSoup
+import requests
+
+import os
+import sys
+
+from lxml import etree
+from lxml.etree import Element, SubElement
+
+apiCallUrl = "http://api.microsofttranslator.com/V2/Http.svc/TranslateArray"
+
+admClient = AdmOAuthClient("","") #client_id and client_secret from MS Azure account
+
+accessToken = admClient.get_access_token()
+
+headers = {
+    'authorization': "Bearer {}".format(accessToken),
+    'content-type': "text/xml",
+    'cache-control': "no-cache",
+    'postman-token': "7d4a7f35-4efe-7c65-bb72-20c78added8a"
+    }
+
+# your raw strings.xml content...of course you can modify this app to accept it from the command line
+xml = """<resources>
+    <string name="network_dialog_title">Connected?</string>
+    <string name="network_dialog_message">It appears there is no network connection.</string>
+    <string name="network_dialog_button_text">OK</string>
+    <!-- main menu options -->
+    <string name="action_refresh">Refresh lists</string>
+    <string name="action_playlist_add">Add playlist</string>
+    <string name="action_settings">Settings</string>
+    <string name="action_news">News</string>
+    <string name="action_about">About</string>
+    <string-array name="tabs">
+        <item>Search</item>
+        <item>Tracks</item>
+        <item>Artists</item>
+        <item>Albums</item>
+        <!-- <item>Playlists</item> -->
+    </string-array>
+    <string name="hello_blank_fragment">Hello blank fragment</string>
+</resources>
+"""
